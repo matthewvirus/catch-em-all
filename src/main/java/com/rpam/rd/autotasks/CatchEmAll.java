6215f53114ad66f0ee29f86a02e678940de04904
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-07-20 20:24:58 +0300

    solved

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -1,17 +1,26 @@
 package com.rpam.rd.autotasks;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CatchEmAll {
 
     //You may set another exception in this field;
-    static Exception exception = new FileNotFoundException();
+    static Exception exception = new FileNotFoundException("fnf");
 
     public static void riskyMethod() throws Exception {
         throw exception;
     }
 
     public static void main(String[] args) throws Exception {
-        riskyMethod();
+        try {
+            riskyMethod();
+        } catch (FileNotFoundException exception) {
+            throw new IllegalArgumentException("Resource is missing", exception);
+        } catch (ArithmeticException | NumberFormatException exception) {
+            System.err.println(exception.getMessage());
+        } catch (IOException exception) {
+            throw new IllegalArgumentException("Resource error", exception);
+        }
     }
 }
